02013a57f122bd68b081cf2d0198b10ffba07a63
Ali01 <alirahanet@gmail.com>
2023-02-07 02:07:43 +0430

    x logo designed

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 let humberger = document.querySelector(".humberger");
+let spanHide = document.querySelector(".humberger .hide");
+let span1 = document.querySelector(".humberger .span-1");
+let span2 = document.querySelector(".humberger .span-2");
 
-console.log(humberger);
+humberger.addEventListener("click",function(event) {
+    event.stopPropagation();
+    spanHide.classList.toggle("hidden");
+    span1.classList.toggle("span-1-j");
+    span2.classList.toggle("span-2-j");
+});
