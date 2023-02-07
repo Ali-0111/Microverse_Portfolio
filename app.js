188b4e346daf64ad644304daa4e0165e92315440
Ali01 <alirahanet@gmail.com>
2023-02-07 23:41:35 +0430

    humberger-menu is done, merge branch

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const spanHide = document.querySelector('.humberger .hide');
 const span1 = document.querySelector('.humberger .span-1');
 const span2 = document.querySelector('.humberger .span-2');
 const links = document.querySelectorAll('nav .flex a');
+const seeProjectBtn = document.querySelector('.btn-4');
 
 // munu-tuggle function
 function menuTuggle() {
@@ -13,7 +14,12 @@ function menuTuggle() {
   span2.classList.toggle('span-2-j');
   mainHead.classList.toggle('main-head-visible');
 }
+function popUp (e) {
+  console.log(e.target);
+}
 // adding events
 humberger.addEventListener('click', menuTuggle);
 // humber ger clicked function
 Array.from(links).forEach((element) => element.addEventListener('click', menuTuggle));
+
+seeProjectBtn.addEventListener('click', popUp);
