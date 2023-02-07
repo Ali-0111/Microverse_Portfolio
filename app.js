c490a6c4d8d3ee797348cfeef2db6c6c52767a56
Ali01 <alirahanet@gmail.com>
2023-02-07 18:02:44 +0430

    correction made for css and js linters

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-let humberger = document.querySelector(".humberger");
-let mainHead = document.querySelector(".main-head-desktop");
-let spanHide = document.querySelector(".humberger .hide");
-let span1 = document.querySelector(".humberger .span-1");
-let span2 = document.querySelector(".humberger .span-2");
-let links = document.querySelectorAll("nav .flex a");
-humberger.addEventListener("click",menu);
+const humberger = document.querySelector('.humberger');
+const mainHead = document.querySelector('.main-head-desktop');
+const spanHide = document.querySelector('.humberger .hide');
+const span1 = document.querySelector('.humberger .span-1');
+const span2 = document.querySelector('.humberger .span-2');
+const links = document.querySelectorAll('nav .flex a');
 
-Array.from(links).forEach((element) =>
-    element.addEventListener("click",menu)
-);
-
-//munu function 
-
-function menu (event) {
-    humberger.classList.toggle("p-fixed");
-    spanHide.classList.toggle("hidden");
-    span1.classList.toggle("span-1-j");
-    span2.classList.toggle("span-2-j");
-    mainHead.classList.toggle("main-head-visible");
+// munu-tuggle function
+function menuTuggle() {
+  humberger.classList.toggle('p-fixed');
+  spanHide.classList.toggle('hidden');
+  span1.classList.toggle('span-1-j');
+  span2.classList.toggle('span-2-j');
+  mainHead.classList.toggle('main-head-visible');
 }
-
-//humber ger clicked function 
-
+// adding events
+humberger.addEventListener('click', menuTuggle);
+// humber ger clicked function
+Array.from(links).forEach((element) => element.addEventListener('click', menuTuggle));
