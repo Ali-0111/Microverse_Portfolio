9159b9ad30213d135e198be04faccf07d318ba67
Ali01 <alirahanet@gmail.com>
2023-02-07 15:55:28 +0430

    menu for humberger click completed

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ let mainHead = document.querySelector(".main-head-desktop");
 let spanHide = document.querySelector(".humberger .hide");
 let span1 = document.querySelector(".humberger .span-1");
 let span2 = document.querySelector(".humberger .span-2");
+let links = document.querySelectorAll("nav .flex a");
+humberger.addEventListener("click",menu);
 
-humberger.addEventListener("click",function(event) {
+Array.from(links).forEach((element) =>
+    element.addEventListener("click",menu)
+);
+
+//munu function 
+
+function menu (event) {
     humberger.classList.toggle("p-fixed");
-    event.stopPropagation();
     spanHide.classList.toggle("hidden");
     span1.classList.toggle("span-1-j");
     span2.classList.toggle("span-2-j");
     mainHead.classList.toggle("main-head-visible");
-});
+}
+
+//humber ger clicked function 
+
