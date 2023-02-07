0f98c6552481139d638e4c2d05b002e91e621d8b
Ali01 <alirahanet@gmail.com>
2023-02-07 13:40:57 +0430

    humberger menu made using css classes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let humberger = document.querySelector(".humberger");
+let mainHead = document.querySelector(".main-head-desktop");
 let spanHide = document.querySelector(".humberger .hide");
 let span1 = document.querySelector(".humberger .span-1");
 let span2 = document.querySelector(".humberger .span-2");
 
 humberger.addEventListener("click",function(event) {
+    humberger.classList.toggle("p-fixed");
     event.stopPropagation();
     spanHide.classList.toggle("hidden");
     span1.classList.toggle("span-1-j");
     span2.classList.toggle("span-2-j");
+    mainHead.classList.toggle("main-head-visible");
 });
